5a68f4a5953a214fbb68edabfcab98e2906e7383
devzonedz <teachingscripts@gmail.com>
2020-12-20 00:47:26 +0100

    INIT

--- index.py
+++ index.py
@@ -11,12 +11,12 @@ env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
 
-password = os.getenv("COMPANY_PASSWORD", "password not found")
+password = os.getenv("PERSONAL_PASSWORD", "password not found")
 
 
-origin = "https://github.com/devzonedz/python-git-automation.git"
+origin = "https://github.com/ScriptsGuy/python-git-automation.git"
 
-push_url = f"https://devzonedz:{password}@github.com/devzonedz/python-git-automation.git"
+push_url = f"https://ScriptsGuy:{password}@github.com/ScriptsGuy/python-git-automation.git"
 
 
 def run(*args):
@@ -25,12 +25,12 @@ def run(*args):
 
 def commit():
     run("add", ".")
-    # run("remote", "add", "origin", origin)
-    # run("remote", "set-url", "origin", origin)
+    run("remote", "add", "origin", origin)
+    run("remote", "set-url", "origin", origin)
     message = input("\nType in your commit message: ")
     commit_message = f"{message}"
     run("commit", "-am", commit_message)
-    # run("push", "--set-upstream", push_url, "master")
+    run("push", "--set-upstream", push_url, "master")
     run("push", push_url)
 
 
