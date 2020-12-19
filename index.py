7865e6b2d60c278b260abb986d18237e1c361e33
devzonedz <teachingscripts@gmail.com>
2020-12-19 23:04:50 +0100

    git python auto

--- index.py
+++ index.py
@@ -10,7 +10,7 @@ def commit():
     run("add", ".")
     message = input("\nType in your commit message: ")
     commit_message = f"{message}"
-    run("commit", "-m", commit_message)
+    run("commit", "-am", commit_message)
     run("push", "-u", "origin", "master")
 
 
