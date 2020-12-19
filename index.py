a01b0ee9e8763f9e14c6a47bb7d5d49635941f63
devzonedz <teachingscripts@gmail.com>
2020-12-19 23:36:57 +0100

    modifeid

--- index.py
+++ index.py
@@ -11,7 +11,7 @@ def commit():
     message = input("\nType in your commit message: ")
     commit_message = f"{message}"
     run("commit", "-am", commit_message)
-    run("push", "-u", "origin", "master")
+    # run("push", "-u", "origin", "master")
 
 
 commit()
